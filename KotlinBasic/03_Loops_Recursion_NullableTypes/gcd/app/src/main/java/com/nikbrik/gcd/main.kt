a303cbeac8c325d677f41670eedb14bcd9ba4b2e
Nikita Shuteev <nikbrik@gmail.com>
2020-12-19 13:39:36 +0300

    HomeWork 03 done

--- KotlinBasic/03_Loops_Recursion_NullableTypes/gcd/app/src/main/java/com/nikbrik/gcd/main.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/gcd/app/src/main/java/com/nikbrik/gcd/main.kt
@@ -1,13 +1,28 @@
 package com.nikbrik.gcd
 
+import kotlin.math.abs
+
 fun main() {
     println("Введите N:")
-    val n = readLine()?.toIntOrNull()
-    if (n==null){
-        println("N не является числом")
-    }else{
-        for (i in 1..n){
-            readLine()?.toIntOrNull()
+    var sum = 0
+    var sumPositiveNumber = 0
+    readLine()?.toIntOrNull()?.let{
+        println("Необходимо ввести $it чисел.")
+        var count = 0
+        while (count<it){
+            println("Введите число ${count+1}:")
+            val number = readLine()?.toIntOrNull()?:continue
+            sum += number
+            sumPositiveNumber += if (number>0) 1 else 0
+            count++
+            println("Число $count принято.")
         }
+        println("Количество введенных положительных чисел: $sumPositiveNumber"
+                +System.lineSeparator())
+        println("Сумма введенных положительных чисел: $sum")
+        println("Введите второе число для вычисления НОД между ним и суммой")
+        println(readLine()?.toIntOrNull()?.let{"НОД: ${gcd(sum,it)}"}?:"Введено некорректное число")
     }
 }
+
+tailrec fun gcd(a:Int, b:Int):Int = if (b == 0) abs(a) else gcd(b, a % b)
