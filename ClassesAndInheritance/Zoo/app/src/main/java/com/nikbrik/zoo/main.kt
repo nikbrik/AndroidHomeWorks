33c1f7ee7aa76627100f8b1c4696eeb330846a51
Никита Шутеев <nikbrik@gmail.com>
2020-12-29 19:31:22 +0300

    HomeWork 05 not done 02

--- ClassesAndInheritance/Zoo/app/src/main/java/com/nikbrik/zoo/main.kt
+++ ClassesAndInheritance/Zoo/app/src/main/java/com/nikbrik/zoo/main.kt
@@ -3,23 +3,23 @@ package com.nikbrik.zoo
 import kotlin.random.Random
 
 fun main() {
-    var newAnimal = Animal("Лошадь", 10, 4, 10)
-    while (true){
-        for (i in 0..Random.nextInt(2)) {
-            newAnimal.move()
-            makeDelay()
-            if(newAnimal.isTooOld)
-                newAnimal = Animal("Лошадь", 10, 4, 10)
-        }
-        newAnimal.eat()
-        if(newAnimal.isTooOld)
-            newAnimal = Animal("Лошадь", 10, 4, 10)
-        makeDelay()
-        newAnimal.sleep()
-        if(newAnimal.isTooOld)
-            newAnimal = Animal("Лошадь", 10, 4, 10)
-        makeDelay()
-    }
+//    var newAnimal = Animal("Лошадь", 10, 4, 10)
+//    while (true){
+//        for (i in 0..Random.nextInt(2)) {
+//            newAnimal.move()
+//            makeDelay()
+//            if(newAnimal.isTooOld)
+//                newAnimal = Animal("Лошадь", 10, 4, 10)
+//        }
+//        newAnimal.eat()
+//        if(newAnimal.isTooOld)
+//            newAnimal = Animal("Лошадь", 10, 4, 10)
+//        makeDelay()
+//        newAnimal.sleep()
+//        if(newAnimal.isTooOld)
+//            newAnimal = Animal("Лошадь", 10, 4, 10)
+//        makeDelay()
+//    }
 }
 
 fun makeDelay(){
