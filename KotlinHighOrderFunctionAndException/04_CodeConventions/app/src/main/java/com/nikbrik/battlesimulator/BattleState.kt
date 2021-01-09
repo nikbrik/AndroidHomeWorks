a30c4e6355884abc5b924211a49343fd896ae775
Nikita Shuteev <nikbrik@gmail.com>
2021-01-09 08:05:24 +0300

    check

--- KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/com/nikbrik/battlesimulator/BattleState.kt
+++ KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/com/nikbrik/battlesimulator/BattleState.kt
@@ -1,17 +1,15 @@
 package com.nikbrik.battlesimulator
 
-sealed class BattleState
-{
-    class    Progress(private val battle: Battle):BattleState() {
+sealed class BattleState {
+
+    class Progress(private val battle: Battle) : BattleState() {
         override fun toString(): String {
             var returnString = "Состояние команды 1:" + System.lineSeparator()
             returnString += battle.getFirstTeamWarriorsState() + System.lineSeparator()
             returnString += "Состояние команды 2:" + System.lineSeparator()
             returnString += battle.getSecondTeamWarriorsState() + System.lineSeparator()
             return returnString
-
         }
-
     }
 
     class FirstTeamWin : BattleState() {
