ea2f7f1e9dfe38553404172696622e322c2eb838
Никита Шутеев <nikbrik@gmail.com>
2021-01-25 18:40:49 +0300

    save

--- BuildAndResources/app/src/main/java/com/nikbrik/BuildAndResources/MainActivity.kt
+++ BuildAndResources/app/src/main/java/com/nikbrik/BuildAndResources/MainActivity.kt
@@ -2,10 +2,20 @@ package com.nikbrik.BuildAndResources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        """flavor = ${BuildConfig.FLAVOR} 
+        |buildType = ${BuildConfig.BUILD_TYPE}
+        |versionCode = ${BuildConfig.VERSION_CODE}
+        |versionName = ${BuildConfig.VERSION_NAME}
+        |applicationId = ${BuildConfig.APPLICATION_ID}
+        |""".trimMargin()
+        .also { findViewById<TextView>(R.id.mainText).text = it }
     }
 }
