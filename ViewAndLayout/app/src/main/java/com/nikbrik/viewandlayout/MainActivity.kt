de2b12ab462aa0e37d40755e1c8f22144baadce5
Nikita Shuteev <nikbrik@gmail.com>
2021-02-01 23:10:26 +0300

    to continue

--- ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.nikbrik.viewandlayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.nikbrik.viewandlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        Glide.with(this)
+            .load("https://images.freeimages.com/images/large-previews/670/snowman-1526046.jpg")
+            .into(binding.helloImage)
     }
 }
