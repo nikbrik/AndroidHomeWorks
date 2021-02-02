c829b193f2f5b3f99330c1ff0df9d334607e6842
Никита Шутеев <nikbrik@gmail.com>
2021-02-02 12:32:15 +0300

    to continue

--- ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nikbrik.viewandlayout
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.nikbrik.viewandlayout.databinding.ActivityMainBinding
@@ -9,10 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        Glide.with(this)
-            .load("https://images.freeimages.com/images/large-previews/670/snowman-1526046.jpg")
-            .into(binding.helloImage)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        binding.imageUrl = "https://images.freeimages.com/images/large-previews/5c6/sunset-jungle-1383333.jpg"
+//        val imageView = findViewById<ImageView>(R.id.hello_image)
+//        val imageUrl =
+////            "https://images.freeimages.com/images/large-previews/670/snowman-1526046.jpg"
+//            "https://images.freeimages.com/images/large-previews/5c6/sunset-jungle-1383333.jpg"
+//
+//        Glide.with(binding.helloImage.context)
+//            .load(binding.imageUrl)
+//            .into(binding.helloImage)
     }
 }
