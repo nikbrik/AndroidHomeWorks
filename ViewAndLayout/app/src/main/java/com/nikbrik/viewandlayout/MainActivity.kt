d18cb5f4bf82e4287fd50443edcda1ac3c42a87c
Никита Шутеев <nikbrik@gmail.com>
2021-02-03 14:29:52 +0300

    to continue

--- ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/nikbrik/viewandlayout/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.nikbrik.viewandlayout
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.bumptech.glide.Glide
 import com.nikbrik.viewandlayout.databinding.ActivityMainBinding
 
@@ -9,11 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        Glide.with(binding.helloImage.context)
-            .load("https://images.freeimages.com/images/large-previews/670/snowman-1526046.jpg")
-            .into(binding.helloImage)
-        setContentView(view)
+        ActivityMainBinding.inflate(layoutInflater).run {
+            val view = root
+            Glide.with(helloImage.context)
+                .load("https://www.freeimages.com/download/file/dfc4fb4259559aded8784be143f3513e/640x480")
+                .into(helloImage)
+            setContentView(view)
+
+            email.addTextChangedListener {
+                updateLoginButton(
+                    loginButton,
+                    email,
+                    password,
+                    agree
+                )
+            }
+            password.addTextChangedListener {
+                updateLoginButton(
+                    loginButton,
+                    email,
+                    password,
+                    agree
+                )
+            }
+            agree.setOnClickListener {
+                updateLoginButton(
+                    loginButton,
+                    email,
+                    password,
+                    agree
+                )
+            }
+        }
     }
+}
+
+inline fun updateLoginButton(button: Button, email: EditText, password: EditText, agree: CheckBox) {
+    button.visibility =
+        if (!email.text.isBlank() && !password.text.isBlank() && agree.isChecked) View.VISIBLE
+        else View.INVISIBLE
 }
