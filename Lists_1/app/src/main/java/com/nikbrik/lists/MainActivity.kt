3649974e7b0b1fef326e873a6add57b6fe1db6dd
Nikita Shuteev <nikbrik@gmail.com>
2021-04-10 22:47:23 +0300

    homework 16 init

--- Lists_1/app/src/main/java/com/nikbrik/lists/MainActivity.kt
+++ Lists_1/app/src/main/java/com/nikbrik/lists/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.nikbrik.lists
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.BuildConfig
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (BuildConfig.DEBUG && Timber.treeCount() == 0) Timber.plant(Timber.DebugTree())
     }
 }
