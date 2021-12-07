e7225e6eac6d471e570e3074e8a14e930a817f12
Nikita Shuteev <nikbrik@gmail.com>
2021-12-07 08:35:02 +0300

    add

--- ContentProvider/ContactBook/app/src/main/java/com/nikbrik/contactbook/ContactsFragment.kt
+++ ContentProvider/ContactBook/app/src/main/java/com/nikbrik/contactbook/ContactsFragment.kt
@@ -1,17 +1,35 @@
 package com.nikbrik.contactbook
 
+import android.Manifest
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.nikbrik.contactbook.databinding.FragmentContactsBinding
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.ktx.constructPermissionsRequest
 
 class ContactsFragment : Fragment(R.layout.fragment_contacts) {
     private val binding: FragmentContactsBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).post {
+            constructPermissionsRequest(
+                Manifest.permission.READ_CONTACTS,
+                onShowRationale = ::onContactPermissionShowRationale,
+                onPermissionDenied = ::onContactPermissionDenied,
+                onNeverAskAgain = ::onContactPermissionNeverAskAgain,
+                requiresPermission = {
+//                    viewModel.loadList()
+                }
+            )
+                .launch()
+        }
 
         initList()
     }
@@ -23,4 +41,17 @@ class ContactsFragment : Fragment(R.layout.fragment_contacts) {
             setHasFixedSize(true)
         }
     }
+
+    private fun onContactPermissionDenied() {
+        toast(R.string.contact_list_permission_denied)
+    }
+
+    private fun onContactPermissionShowRationale(request: PermissionRequest) {
+        request.proceed()
+    }
+
+    private fun onContactPermissionNeverAskAgain() {
+        toast(R.string.contact_list_permission_never_ask_again)
+    }
+
 }
